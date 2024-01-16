666e2eef94d83a10c34ba7f0bcea35944262189e
aimadjaouhar1 <aimadjaouhar@icloud.com>
2024-01-16 20:58:40 +0100

    feat: add page title to navbar

--- apps/web/src/app/_core/components/navbar/navbar.component.ts
+++ apps/web/src/app/_core/components/navbar/navbar.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { NavigationService } from '@web/app/services/navigation.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, TranslateModule],
+  imports: [RouterLink, TranslateModule, AsyncPipe],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+
+  navigationService = inject(NavigationService);
+
+  title$ = this.navigationService.getRouteChange$().pipe(map((route => route?.data!['title'])));
+
 }
