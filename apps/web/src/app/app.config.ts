54aba4970dfaee6494883e1b7f2fc0bd42655db7
aimadjaouhar1 <aimadjaouhar@icloud.com>
2024-01-16 15:24:19 +0100

    feat(ng-translate): install and configure initial translation

--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -1,7 +1,29 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { environment } from 'apps/web/src/environments/environment';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
+export function translateLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, `./assets/i18n/`, '.json');
+}
+
+export const provideTranslation = () => ({
+  defaultLanguage: environment.defaultLanguage,
+  loader: {
+    provide: TranslateLoader,
+    useFactory: translateLoaderFactory,
+    deps: [HttpClient],
+  },
+});
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [
+    provideRouter(appRoutes),
+    provideHttpClient(withFetch()),
+    importProvidersFrom(TranslateModule.forRoot(provideTranslation()))
+  ],
 };
