30257ef8e5d07b0a84af0f4b1964912459d7e1b7
aimadjaouhar1 <aimadjaouhar@icloud.com>
2024-01-18 00:10:35 +0100

    feat: create pdfeditor service, load pdf doc & pages

---
+++ apps/web/src/app/_core/services/pdf-editor.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { Observable, forkJoin, from } from "rxjs";
+import { PDFDocumentProxy, PDFPageProxy } from "pdfjs-dist";
+import * as pdfjs from 'pdfjs-dist';
+
+
+
+@Injectable({ providedIn: 'root' })
+export class PdfEditorService {  
+
+    loadPdfDocument(pdfFile: string): Observable<PDFDocumentProxy> {
+        return from(pdfjs.getDocument(pdfFile).promise);
+    }
+
+    loadPdfPage(pdf: PDFDocumentProxy, pageNum: number): Observable<PDFPageProxy> {
+       return from(pdf.getPage(pageNum))
+    }
+
+    loadPdfPages(pdf: PDFDocumentProxy, pageNumFrom: number, pageNumTo: number): Observable<PDFPageProxy[]> {
+        const observables: Observable<PDFPageProxy>[] = [];
+
+        for (let pageNum = pageNumFrom; pageNum <= pageNumTo; pageNum++) {
+            observables.push(from(pdf.getPage(pageNum)));
+        }
+        
+        return forkJoin(observables);
+    }
+}
