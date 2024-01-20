644687c8f55cbe7a32b846b3e5328ee72cb5407b
aimadjaouhar1 <aimadjaouhar@icloud.com>
2024-01-20 18:49:32 +0100

    chore: Add build.sh file: a shell script to automate project build, Docker build, and generation of the current version's docker-compose.prod file

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+read -p "Please enter the version number (e.g., 0.1.0): " version_number
+
+if ! [[ $version_number =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "Error: Invalid version number format. Please use the format 0.0.0"
+    exit 1
+fi
+
+echo "Removing dist directory..."
+rm -R dist
+
+echo "Building web project..."
+npx nx build web 
+
+echo "Building Docker image for web..."
+docker build -t "aimadjaouhar/org.pdfmaster:web-$version_number" -f docker/web.Dockerfile . --no-cache
+
+echo "Pushing Docker image web..."
+docker push "aimadjaouhar/org.pdfmaster:web-$version_number"
+
+sed "s/\${version}/$version_number/g" docker-compose.example.yml > tmp && mv tmp docker-compose.prod.yml
+
+
