d42966ffa6589b10be056517f26c4856a380b18f
aimadjaouhar1 <aimadjaouhar@icloud.com>
2024-02-02 22:33:29 +0100

    feat: change jwt strategy, extract from cookies

--- apps/api/src/auth/jwt.strategy.ts
+++ apps/api/src/auth/jwt.strategy.ts
@@ -2,15 +2,30 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
+import { Request as RequestType } from 'express';
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
     
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        JwtStrategy.extractJWTFromCookies,
+      ]),
       ignoreExpiration: false,
       secretOrKey: `${process.env.API_JWT_SECRET}`,
     });
+  }
+
+  private static extractJWTFromCookies(req: RequestType): string | null {
+    if (
+      req.cookies &&
+      'token' in req.cookies &&
+      req.cookies.token.length > 0
+    ) {
+      return req.cookies.token;
+    }
+    return null;
   }
 
   async validate(payload: unknown) {
