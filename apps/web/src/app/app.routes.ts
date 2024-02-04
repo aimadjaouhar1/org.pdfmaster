06875248f64d3943192a8e29c4a26301c8b66359
aimadjaouhar1 <aimadjaouhar@icloud.com>
2024-02-05 00:12:58 +0100

    feat: add 404 page

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,11 +1,18 @@
 import { Route } from '@angular/router';
 import { CustomTitleResolver } from '@web/app/resolvers/custom-title.resolver';
+import { PageNotFoundComponent } from '@web/shared/components/page-not-found/page-not-found.component';
 
 export const appRoutes: Route[] = [
     {
         path: 'edit',
         title: CustomTitleResolver,
         data: { title: 'TITLES.PAGE_EDIT'},
-        loadComponent: () => import('@web/features/pdf-edit/pdf-edit.component').then(m => m.PdfEditComponent)
+        loadComponent: () => import('@web/features/pdf-edit/pdf-edit.component').then(m => m.PdfEditComponent),
+    },
+    { 
+        path: '**', 
+        title: CustomTitleResolver,
+        pathMatch: 'full',  
+        component: PageNotFoundComponent
     }
 ];
