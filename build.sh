ad5eca9ea7bec58845fdd9226d9f211e2addaa93
aimadjaouhar1 <aimadjaouhar@icloud.com>
2024-02-05 01:50:44 +0100

    chore(scripts): add build api to build.sh

--- build.sh
+++ build.sh
@@ -13,12 +13,19 @@ rm -R dist
 echo "Building web project..."
 npx nx build web 
 
+echo "Building api project..."
+npx nx build api 
+
 echo "Building Docker image for web..."
 docker build -t "aimadjaouhar/org.pdfmaster:web-$version_number" -f docker/web.Dockerfile . --no-cache
+
+echo "Building Docker image for api..."
+docker build -t "aimadjaouhar/org.pdfmaster:api-$version_number" -f docker/api.Dockerfile . --no-cache
 
 echo "Pushing Docker image web..."
 docker push "aimadjaouhar/org.pdfmaster:web-$version_number"
 
+echo "Pushing Docker image api..."
+docker push "aimadjaouhar/org.pdfmaster:api-$version_number"
+
 sed "s/\${version}/$version_number/g" docker-compose.prod.example.yml > tmp && mv tmp docker-compose.prod.yml
-
-
