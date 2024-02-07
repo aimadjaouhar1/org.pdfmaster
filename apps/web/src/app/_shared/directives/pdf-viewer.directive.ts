1f440627ea778e166bd79e9e6b3a29c1a335144a
aimadjaouhar1 <aimadjaouhar@icloud.com>
2024-02-07 01:42:07 +0100

    refactor: PdfViewerDirective  calculate scale when viewportParameters is null

--- apps/web/src/app/_shared/directives/pdf-viewer.directive.ts
+++ apps/web/src/app/_shared/directives/pdf-viewer.directive.ts
@@ -9,7 +9,7 @@ import { GetViewportParameters } from 'pdfjs-dist/types/src/display/api';
 export class PdfViewerDirective implements OnChanges {
 
   @Input({required: true}) page?: PDFPageProxy;
-  @Input({required: true}) viewportParameters?: GetViewportParameters;
+  @Input() viewportParameters?: GetViewportParameters;
 
   @Input() width?: number | null;
   @Input() height?: number | null;
@@ -24,8 +24,14 @@ export class PdfViewerDirective implements OnChanges {
       const canvas: HTMLCanvasElement = this.el.nativeElement as HTMLCanvasElement;
       const context = canvas.getContext('2d');
 
+      const _scale = this.width! / this.page!.getViewport({scale: 1}).width;
+
+      if(!this.viewportParameters) {
+       this.viewportParameters = { scale: _scale };
+      } 
+      
       const viewport = page.getViewport(this.viewportParameters);
-    
+
       canvas.width = this.width || viewport.width;
       canvas.height = this.height || viewport.height;
       
