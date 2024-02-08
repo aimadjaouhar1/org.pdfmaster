7f9acb0a0e9ec1c5b05ae2f724229bca3aeb4148
aimadjaouhar1 <aimadjaouhar@icloud.com>
2024-02-08 12:31:33 +0100

    feat: add page selection, action buttons

--- apps/web/src/app/_shared/components/pdf-page-list/pdf-page-list.component.ts
+++ apps/web/src/app/_shared/components/pdf-page-list/pdf-page-list.component.ts
@@ -1,16 +1,60 @@
-import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule } from '@ngx-translate/core';
 import { PdfViewerDirective } from '@web/shared/directives/pdf-viewer.directive';
 import { PDFPageProxy } from 'pdfjs-dist';
+
+enum PageActionButton {
+  DELETE = 'delete',
+  PREVIEW = 'preview',
+  DUPLICATE = 'duplicate'
+}
 
 @Component({
   selector: 'app-pdf-page-list',
   standalone: true,
-  imports: [PdfViewerDirective],
+  imports: [NgClass, FormsModule, PdfViewerDirective, NgbTooltip, TranslateModule],
   templateUrl: './pdf-page-list.component.html',
   styleUrl: './pdf-page-list.component.scss',
 })
-export class PdfPageListComponent {
+export class PdfPageListComponent implements OnChanges {
+  @Input({ required: true }) pages?: PDFPageProxy[] | Array<PDFPageProxy & {selected: boolean}> | null;
 
-  @Input() pages?: PDFPageProxy[] | null;  
+  @Input() pageActionButtons?: PageActionButton[] | ('delete' | 'preview' | 'duplicate')[];
+  @Input() selection: boolean = false;
 
+  @Output() preview = new EventEmitter<PDFPageProxy>();
+  @Output() duplicate = new EventEmitter<PDFPageProxy>();
+  @Output() delete = new EventEmitter<number>();
+  @Output() selectedPagesChange = new EventEmitter<number[]>();
+
+  showDelete = false;
+  showDuplicate = false;
+  showPreview = false;
+  showActionBtns =  false;
+
+  pagesSelection?: boolean[];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['pageActionButtons']?.currentValue) {
+      this.showDelete = this.pageActionButtons!.includes(PageActionButton.DELETE);
+      this.showDuplicate = this.pageActionButtons!.includes(PageActionButton.DUPLICATE);
+      this.showPreview = this.pageActionButtons!.includes(PageActionButton.PREVIEW);
+      this.showActionBtns = this.pageActionButtons!.length > 0;
+    }
+
+    if(this.selection && changes['pages']?.currentValue) {
+      this.pagesSelection = Array(this.pages?.length).fill(false);
+    }
+  }
+
+  onClickPreview = (page: PDFPageProxy) => this.preview.emit(page);
+
+  onClickDuplicate = (page: PDFPageProxy) => this.duplicate.emit(page);
+
+  onClickDelete = (index: number) => this.delete.emit(index);
+
+  onSelectPageChange = () =>  this.selectedPagesChange.emit(this.pagesSelection!.filter(selected => selected).map((_, index) => index));
 }
