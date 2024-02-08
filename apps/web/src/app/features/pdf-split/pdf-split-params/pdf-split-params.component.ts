3d8c378112c2750e71a59f237fa52680373a0769
aimadjaouhar1 <aimadjaouhar@icloud.com>
2024-02-08 01:30:13 +0100

    refactor(@web): change to reactive form, add validators to pdf-split-params

--- apps/web/src/app/features/pdf-split/pdf-split-params/pdf-split-params.component.ts
+++ apps/web/src/app/features/pdf-split/pdf-split-params/pdf-split-params.component.ts
@@ -1,26 +1,43 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-pdf-split-params',
   standalone: true,
-  imports: [TranslateModule, FormsModule],
+  imports: [NgClass, TranslateModule, ReactiveFormsModule],
   templateUrl: './pdf-split-params.component.html',
   styleUrl: './pdf-split-params.component.scss',
 })
-export class PdfSplitParamsComponent {
+export class PdfSplitParamsComponent implements OnChanges {
 
   @Input({required: true}) pdfPages!: number;
 
   @Output() split = new EventEmitter<number>();
 
-  interval!: number;
   pdfCount: number = 0;
 
-  onClickSplit = () => this.split.emit(this.interval);
+  formBuilder = inject(FormBuilder);
+  
+  splitParamsForm = this.formBuilder.group({
+    interval: [1],
+  });
+
+  get controls() {
+    return this.splitParamsForm?.controls;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['pdfPages'].currentValue) {
+      this.controls.interval.addValidators(Validators.max(this.pdfPages));
+      this.updateValues();
+    }
+  }
+  onClickSplit = () =>  this.splitParamsForm.valid ? this.split.emit(this.splitParamsForm.getRawValue().interval!) : undefined;
  
   updateValues() {
-    this.pdfCount= this.interval ?  Math.ceil(this.pdfPages / this.interval) : 0;
+    const interval = this.controls.interval.value;
+    this.pdfCount = interval ?  Math.ceil(this.pdfPages / interval) : 0;
   }
 }
