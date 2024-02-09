bf53d0e8ca20f0433196b300b4df15d11f0a2d5e
aimadjaouhar1 <aimadjaouhar@icloud.com>
2024-02-09 01:26:59 +0100

    feat: add page list

--- apps/web/src/app/features/pdf-extract/pdf-extract.component.ts
+++ apps/web/src/app/features/pdf-extract/pdf-extract.component.ts
@@ -1,14 +1,18 @@
+import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { FileMimeType } from '@shared-lib/enums';
 import { PdfHttp } from '@web/app/http/pdf.http';
 import { PdfEditorService } from '@web/app/services/pdf-editor.service';
 import { FileUploadDropzoneComponent } from '@web/shared/components/file-upload-dropzone/file-upload-dropzone.component';
+import { PdfPageListComponent } from '@web/shared/components/pdf-page-list/pdf-page-list.component';
+import { PDFPageProxy } from 'pdfjs-dist';
+import { Observable, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-pdf-extract',
   standalone: true,
-  imports: [FileUploadDropzoneComponent],
+  imports: [AsyncPipe, FileUploadDropzoneComponent, PdfPageListComponent],
   templateUrl: './pdf-extract.component.html',
   styleUrl: './pdf-extract.component.scss'
 })
@@ -21,9 +25,15 @@ export class PdfExtractComponent {
   readonly accept = [FileMimeType.PDF];
 
   pdfFile?: File;
+  loadedPdfPages$?: Observable<PDFPageProxy[]>;
+
 
   async onSelectFiles(files: File[]) {
     this.pdfFile = files[0];
+
+    this.loadedPdfPages$ = this.pdfEditorService.loadPdfDocument(await this.pdfFile.arrayBuffer())
+      .pipe(switchMap(pdfDoc => this.pdfEditorService.loadPdfPages(pdfDoc, 1, pdfDoc.numPages))
+    );
   }
 
 }
