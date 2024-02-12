a1c0c539c05374728e655f8b3f6d42216be9e6da
aimadjaouhar1 <aimadjaouhar@icloud.com>
2024-02-12 10:03:30 +0100

    feat: add PageState and TextLayerState type

--- apps/web/src/app/_core/types/pdf-editor.types.ts
+++ apps/web/src/app/_core/types/pdf-editor.types.ts
@@ -1,5 +1,8 @@
 export type Pagination = {page: number, limit: number};
 export type FabriCanvasState = { version: string; objects: fabric.Object[] };
+export type TextLayerState = { deleted?: boolean[] };
+export type PageState = {fabricCanvasState: FabriCanvasState, textLayerState: TextLayerState};
+
 
 export type ToolData = {cursor: string, type: string};
 export type TextBoxOptions = {font: string, size: number, color: string};
